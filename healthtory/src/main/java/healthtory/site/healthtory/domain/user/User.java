f2f08a13f89fa3aa44596c4386e6924a286ff0e7
JiWonKim <hghg1036@naver.com>
2023-07-07 11:27:28 +0900

    feat: 회원가입 기능 구현

--- healthtory/src/main/java/healthtory/site/healthtory/domain/user/User.java
+++ healthtory/src/main/java/healthtory/site/healthtory/domain/user/User.java
@@ -1,7 +1,9 @@
 package healthtory.site.healthtory.domain.user;
 
 import java.sql.Timestamp;
+import java.util.Date;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -16,10 +18,25 @@ public class User {
     private String nickname;
     private String age;
     private String phoneNumber;
-    private Integer birthday;
+    private Date birthday;
     private String role;
     private String gender;
     private String profileImg;
 	private Timestamp createdAt;
-	private Timestamp updatedAt;	
+    private Timestamp updatedAt;
+    
+    @Builder
+    public User(String loginId, String password, String name, String email, String nickname, String age,
+            String phoneNumber, Date birthday, String gender) {
+                super();
+                this.loginId = loginId;
+                this.password = password;
+                this.name = name;
+                this.email = email;
+                this.nickname = nickname;
+                this.age = age;
+                this.phoneNumber = phoneNumber;
+                this.birthday = birthday;
+                this.gender = gender;
+            }
 }
--- healthtory/src/main/java/healthtory/site/healthtory/service/UserService.java
+++ healthtory/src/main/java/healthtory/site/healthtory/service/UserService.java
@@ -2,7 +2,22 @@ package healthtory.site.healthtory.service;
 
 import org.springframework.stereotype.Service;
 
+import healthtory.site.healthtory.domain.user.User;
+import healthtory.site.healthtory.domain.user.UserDao;
+import healthtory.site.healthtory.web.dto.request.user.JoinReqDto;
+import lombok.RequiredArgsConstructor;
+@RequiredArgsConstructor
 @Service
 public class UserService {
+
+    private final UserDao userDao;
+    
+    public JoinReqDto join(JoinReqDto joinReqDto) {
+        userDao.insert(joinReqDto.toUser());
+        User userPS = userDao.findByUser(joinReqDto.getLoginId());
+        JoinReqDto joinRespDto = new JoinReqDto(userPS);
+        return joinRespDto;
+    }
+
     
 }
