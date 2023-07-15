432b0b9bada1b291e092fb106dd02387d11dfb2a
JiWonKim <hghg1036@naver.com>
2023-07-16 08:15:32 +0900

    Remove: EditReqDto 삭제

--- healthtory/src/main/java/healthtory/site/healthtory/web/UserController.java
+++ healthtory/src/main/java/healthtory/site/healthtory/web/UserController.java
@@ -4,7 +4,6 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 import healthtory.site.healthtory.domain.user.User;
 import healthtory.site.healthtory.service.UserService;
 import healthtory.site.healthtory.web.dto.CMRespDto;
-import healthtory.site.healthtory.web.dto.request.user.EditReqDto;
 import healthtory.site.healthtory.web.dto.request.user.JoinReqDto;
 import healthtory.site.healthtory.web.dto.request.user.LoginReqDto;
 import healthtory.site.healthtory.web.dto.response.SessionUserDto;
