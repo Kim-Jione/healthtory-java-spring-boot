76d0d3eb8c6e663508fdfe989a37e30edcf07061
kim_jiwon <106166409+Kim-Jione@users.noreply.github.com>
2023-07-31 15:50:13 +0900

    Refactor: join 기능 수정 (#5)
    - 기존 회원가입 방식에서 관심사 선택기능 추가
    - 위 기능에 해당하는 테이블 추가

---
+++ healthtory/src/main/java/healthtory/site/healthtory/domain/user_interest/UserInterest.java
@@ -0,0 +1,17 @@
+package healthtory.site.healthtory.domain.user_interest;
+
+import java.sql.Timestamp;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class UserInterest {
+    private Integer userInterestId;
+	private Integer userId;
+	private Integer categoryId;
+	private String interestTitle;
+	private Timestamp createdAt;
+	private Timestamp updatedAt;
+}
--- healthtory/src/main/java/healthtory/site/healthtory/web/dto/request/user/JoinReqDto.java
+++ healthtory/src/main/java/healthtory/site/healthtory/web/dto/request/user/JoinReqDto.java
@@ -1,8 +1,10 @@
 package healthtory.site.healthtory.web.dto.request.user;
 
 import java.util.Date;
+import java.util.List;
 
 import healthtory.site.healthtory.domain.user.User;
+import healthtory.site.healthtory.domain.user_interest.UserInterest;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -14,24 +16,31 @@ public class JoinReqDto {
     private String loginId;
     private String password;
     private String email;
+    private String interest;
     private String nickname;
+    private String interestTitle;
     private Integer age;
     private Date birthday;
     private String gender;
+    private List<Integer> categoryIdList;
+	private List<String> userInterestList;
+
 
     public User toUser() {
-        return User.builder().loginId(this.loginId).password(this.password).email(this.email)
+        return User.builder().loginId(this.loginId).password(this.password).email(this.email).insert(this.interest)
                 .nickname(this.nickname).age(this.age).birthday(this.birthday)
                 .gender(this.gender).build();
     }
 
     public JoinReqDto(User userPS) {
-                this.loginId = userPS.getLoginId();
-                this.password = userPS.getPassword();
-                this.email = userPS.getEmail();
-                this.nickname = userPS.getNickname();
-                this.age = userPS.getAge();
-                this.birthday = userPS.getBirthday();
-                this.gender = userPS.getGender();
+        this.loginId = userPS.getLoginId();
+        this.password = userPS.getPassword();
+        this.email = userPS.getEmail();
+        this.interest = userPS.getInterest();
+        this.nickname = userPS.getNickname();
+        this.age = userPS.getAge();
+        this.birthday = userPS.getBirthday();
+        this.gender = userPS.getGender();
     }
+
 }
