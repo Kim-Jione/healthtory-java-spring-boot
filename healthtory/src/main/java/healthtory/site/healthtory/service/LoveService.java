7e120e4cd939788f9b5ac4132cc82530094d5553
kim_jiwon <106166409+Kim-Jione@users.noreply.github.com>
2023-08-15 03:03:23 +0900

    Feat: Subscribe 구독, 좋아요 기능 구현 (#8)
    - 구독 기능
    - 구독 취소 기능
    - 좋아요 기능
    - 좋아요 취소 기능
    - Resp 데이터 리펙토링

--- healthtory/src/main/java/healthtory/site/healthtory/service/LoveService.java
+++ healthtory/src/main/java/healthtory/site/healthtory/service/LoveService.java
@@ -2,7 +2,32 @@ package healthtory.site.healthtory.service;
 
 import org.springframework.stereotype.Service;
 
+import healthtory.site.healthtory.domain.love.Love;
+import healthtory.site.healthtory.domain.love.LoveDao;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Service
 public class LoveService {
-    
+
+    private final LoveDao loveDao;
+
+    public Love love(Integer userId, Integer postId) {
+        Love love = new Love(userId, postId);
+        loveDao.insert(love);
+        Integer loveId = loveDao.findByLoveId(love.getUserId(), love.getPostId());
+        Love lovePS = loveDao.findById(loveId);
+        return lovePS;
+    }
+
+    public Integer findById(Integer userId, Integer postId) {
+        return loveDao.findByLoveId(userId, postId);
+    }
+
+    public Love unLove(Integer loveId) {
+        Love lovePS = loveDao.findById(loveId);
+        loveDao.delete(lovePS.getLoveId());
+        return lovePS;
+    }
+
 }
